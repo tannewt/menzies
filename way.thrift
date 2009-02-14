9b2f176119be39fce402e55093dc2eb036088348
jakin <jason.kivlighn@gmail.com>
2009-02-14 15:28:27 -0800

    I mostly working way server according to the current design

--- way.thrift
+++ way.thrift
@@ -1,9 +1,8 @@
 include "data.thrift"
 
 service WayServer {
-  data.Way getWay(1: i64 id),
+	data.Way getWay(1: i64 id),
 	data.Way getWayVersion(1: i64 id, 2: i32 version)
-	data.Osm getFullWay(1: i64 id),
 	list<data.Way> getWays(1: list<i64> ids),
 	list<data.Way> getWaysFromNode(1: i64 node),
 	list<data.Way> getWaysFromRelation(1: i64 relation),
@@ -12,3 +11,4 @@ service WayServer {
 	i64 createWay(1: data.Way way),
 	list<data.Way> wayHistory(1: i64 id)
 }
+
