1b9c56817081fa83a52779769983653d2cd8f944
Scott Shawcroft <scott.shawcroft@gmail.com>
2009-02-27 13:59:31 -0800

    Rectangle viewer.  Only shows 10,000 because its slow.  May be different with less skinny rectangles.

---
+++ test/rectangles.py
@@ -0,0 +1,82 @@
+import sys
+import os
+sys.path.append(os.getcwd())
+
+from bsddb import db as bdb
+
+db = bdb.DB()
+db.open(sys.argv[1],"Spatial Index", bdb.DB_BTREE, 0)
+
+import goocanvas
+import gtk
+import cairo
+
+window = gtk.Window()
+window.connect("destroy", lambda x: gtk.main_quit())
+window.show()
+
+vbox = gtk.VBox()
+vbox.show()
+
+canvas = goocanvas.Canvas()
+canvas.set_bounds(0, 0, 360, 180)
+root = canvas.get_root_item()
+canvas.show()
+
+scrolled = gtk.ScrolledWindow()
+scrolled.show()
+scrolled.add(canvas)
+
+vbox.pack_start(scrolled)
+
+hbox = gtk.HBox()
+hbox.show()
+vbox.pack_start(hbox,False,False)
+zoom = gtk.HScale()
+zoom.set_range(1.0,100.0)
+zoom.connect("value-changed",lambda r: canvas.set_scale(r.get_value()))
+zoom.set_value(1.0)
+zoom.show()
+hbox.pack_start(zoom)
+
+window.add(vbox)
+
+rect = goocanvas.Rect(parent=root, x=0, y=0, width=360, height=180, stroke_color_rgba=0x000000ff, line_width=1, line_dash=goocanvas.LineDash([1.0, 1.0]))
+
+splits = int(float(db.get("splits")))
+
+layers = {}
+for layer in [0]:
+	layers[layer] = []
+	for x in range(splits)[:100]:
+		for y in range(splits)[:100]:
+			bounds = db.get("%d-%d-%d" % (layer,x,y))
+			if bounds == None:
+				continue
+			min_lat, min_lon, max_lat, max_lon = map(float, bounds.split(":"))
+			min_lat += 90
+			max_lat += 90
+			min_lon += 180
+			max_lon += 180
+			
+			min_lat = 180 - min_lat
+			max_lat = 180 - max_lat
+			
+			width = max_lon-min_lon
+			height = min_lat-max_lat
+			
+			#print min_lat, min_lon, width, height
+			rect = goocanvas.Rect(parent=root, x=min_lon, y=min_lat, width=width, height=height, fill_color_rgba=0x000000f0, line_width=0)
+			layers[layer].append(rect)
+
+def output(button):
+	img = cairo.ImageSurface(cairo.FORMAT_ARGB32,3600,1800)
+	context = cairo.Context(img)
+	canvas.render(context,None,10.0)
+	img.write_to_png("output.png")
+
+out = gtk.Button("Output")
+out.show()
+out.connect("clicked", output)
+hbox.pack_start(out,False,False)
+gtk.main()
