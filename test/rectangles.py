62f28e8e09892cc660067e6139b1ebe4de378cff
Scott Shawcroft <scott.shawcroft@gmail.com>
2009-02-27 16:30:04 -0800

    Click and drag to zoom.

--- test/rectangles.py
+++ test/rectangles.py
@@ -19,12 +19,22 @@ window = gtk.Window()
 window.connect("destroy", lambda x: gtk.main_quit())
 window.show()
 
+hbox2 = gtk.HBox()
+hbox2.show()
+
+level = gtk.VScale()
+level.show()
+
+hbox2.pack_start(level, False, False)
+
 vbox = gtk.VBox()
 vbox.show()
+hbox2.pack_start(vbox)
 
 canvas = goocanvas.Canvas()
 canvas.set_bounds(0, 0, 360, 180)
 root = canvas.get_root_item()
+
 canvas.show()
 
 scrolled = gtk.ScrolledWindow()
@@ -37,17 +47,61 @@ hbox = gtk.HBox()
 hbox.show()
 vbox.pack_start(hbox,False,False)
 zoom = gtk.HScale()
-zoom.set_range(1.0,100.0)
+zoom.set_range(1.0,10000.0)
 zoom.connect("value-changed",lambda r: canvas.set_scale(r.get_value()))
 zoom.set_value(1.0)
 zoom.show()
 hbox.pack_start(zoom)
 
-window.add(vbox)
+window.add(hbox2)
 
 rect = goocanvas.Rect(parent=root, x=0, y=0, width=360, height=180, stroke_color_rgba=0x000000ff, line_width=1, line_dash=goocanvas.LineDash([1.0, 1.0]))
 
 num_layers = int(db.get("levels"))+1
+
+level.set_range(0,num_layers-1)
+current_layer = num_layers-1
+level.set_value(num_layers-1)
+level.set_inverted(True)
+level.set_increments(1,5)
+
+select = goocanvas.Rect(parent=root, x=0, y=0, width=360, height=180, fill_color_rgba=0xff000090, line_width=0, visibility=goocanvas.ITEM_HIDDEN)
+start_x = None
+start_y = None
+end_x = None
+end_y = None
+
+def down(item, target, event):
+	global select, start_x, start_y
+	if event.button == 1:
+		start_x = event.x
+		start_y = event.y
+
+def up(item, target, event):
+	global select, start_x, start_y, end_x, end_y, zoom
+	if event.button == 1:
+		select.props.visibility = goocanvas.ITEM_HIDDEN
+		vscale = scrolled.props.vadjustment.page_size/abs(start_y - end_y)
+		hscale = scrolled.props.hadjustment.page_size/abs(start_x - end_x)
+		scale = min(hscale,vscale)
+		zoom.set_value(scale)
+		scrolled.props.hadjustment.value = scale*min(start_x, end_x)
+		scrolled.props.vadjustment.value = scale*min(start_y, end_y)
+
+def move(item, target, event):
+	global select, start_x, start_y, end_x, end_y
+	if event.get_state() & gtk.gdk.BUTTON1_MASK:
+		select.props.visibility = goocanvas.ITEM_VISIBLE
+		end_x = event.x
+		end_y = event.y
+		select.props.x = min(start_x, end_x)
+		select.props.width = abs(start_x - end_x)
+		select.props.y = min(start_y, end_y)
+		select.props.height = abs(start_y - end_y)
+
+root.connect("button-press-event",down)
+root.connect("button-release-event",up)
+root.connect("motion-notify-event",move)
 
 layers = {}
 for layer in range(num_layers):
@@ -71,7 +125,9 @@ for layer in range(num_layers):
 			height = min_lat-max_lat
 			
 			#print min_lat, min_lon, width, height
-			rect = goocanvas.Rect(parent=root, x=min_lon, y=min_lat, width=width, height=height, fill_color_rgba=0x000000f0, line_width=0)
+			rect = goocanvas.Rect(parent=root, x=min_lon, y=min_lat-height, width=width, height=height, fill_color_rgba=0x000000f0, line_width=0)
+			if layer!=current_layer:
+				rect.props.visibility = goocanvas.ITEM_HIDDEN
 			layers[layer].append(rect)
 
 def output(button):
@@ -79,6 +135,18 @@ def output(button):
 	context = cairo.Context(img)
 	canvas.render(context,None,10.0)
 	img.write_to_png("output.png")
+
+def change_level(scale, scroll, value):
+	global current_layer
+	value = int(value)
+	scale.set_value(value)
+	for rect in layers[current_layer]:
+		rect.props.visibility = goocanvas.ITEM_HIDDEN
+	current_layer = value
+	for rect in layers[value]:
+		rect.props.visibility = goocanvas.ITEM_VISIBLE
+	
+level.connect("change-value", change_level)
 
 out = gtk.Button("Output")
 out.show()
