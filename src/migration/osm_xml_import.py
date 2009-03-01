0e3a946b6172dd4593497218cca17a1f5c7a0170
jakin <jason.kivlighn@gmail.com>
2009-02-28 18:26:32 -0800

    Import only data that a particular server cares about.

--- src/migration/osm_xml_import.py
+++ src/migration/osm_xml_import.py
@@ -6,10 +6,17 @@ import bz2
 
 from xml.sax import make_parser, handler
 
+THIS_SERVER = 2 # less than 0 for a way or relation server
+LOAD_WAYS = False
+LOAD_RELATIONS = False
+
 sys.path.append('common')
 sys.path.append('common/gen-py')
 from data.ttypes import *
 
+sys.path.append('front')
+from partitioner import *
+node_partitioner = StaticLatPartitioner(range(4))
 raw_spatial_data = open("spatial_data.raw", "w")
 
 sys.path.append('nodebox')
@@ -43,9 +50,12 @@ class FancyCounter(handler.ContentHandler):
 			self._attr_types[n] = self._attr_types.get(n, 0) + 1
 
 		if name == "node":
+			self._object = None
+			if THIS_SERVER < 0: return
+
 			self._object = Node()
 			self._object.tags = {}
-			self._object.id = int(attrs["id"])
+			self._object.id = long(attrs["id"])
 			self._object.lat = float(attrs["lat"])
 			self._object.lon = float(attrs["lon"])
 
@@ -56,10 +66,13 @@ class FancyCounter(handler.ContentHandler):
 			if attrs.has_key("version"): self._object.version = int(attrs["version"])
 			if attrs.has_key("timestamp"): self._object.timestamp = time.mktime(time.strptime(attrs["timestamp"], "%Y-%m-%dT%H:%M:%SZ"))
 		elif name == "way":
+			self._object = None
+			if not LOAD_WAYS: return
+
 			self._object = Way()
 			self._object.tags = {}
 			self._object.nodes = []
-			self._object.id = int(attrs["id"])
+			self._object.id = long(attrs["id"])
 			if attrs.has_key("changeset"): self._object.changeset = int(attrs["changeset"])
 			if attrs.has_key("visible"): self._object.visible = attrs["visible"] == "true"
 			if attrs.has_key("user"): self._object.user = attrs["user"].encode("utf-8")
@@ -67,10 +80,13 @@ class FancyCounter(handler.ContentHandler):
 			if attrs.has_key("version"): self._object.version = int(attrs["version"])
 			if attrs.has_key("timestamp"): self._object.timestamp = time.mktime(time.strptime(attrs["timestamp"], "%Y-%m-%dT%H:%M:%SZ"))
 		elif name == "relation":
+			self._object = None
+			if not LOAD_RELATIONS: return
+
 			self._object = Relation()
 			self._object.tags = {}
 			self._object.members = set()
-			self._object.id = int(attrs["id"])
+			self._object.id = long(attrs["id"])
 			if attrs.has_key("changeset"): self._object.changeset = int(attrs["changeset"])
 			if attrs.has_key("visible"): self._object.visible = attrs["visible"] == "true"
 			if attrs.has_key("user"): self._object.user = attrs["user"].encode("utf-8")
@@ -78,10 +94,14 @@ class FancyCounter(handler.ContentHandler):
 			if attrs.has_key("version"): self._object.version = int(attrs["version"])
 			if attrs.has_key("timestamp"): self._object.timestamp = time.mktime(time.strptime(attrs["timestamp"], "%Y-%m-%dT%H:%M:%SZ"))
 		elif name == "tag":
+			if self._object == None: return
 			self._object.tags[attrs["k"].encode("utf-8")] = attrs["v"].encode("utf-8")
 		elif name == "nd":
+			if self._object == None: return
 			self._object.nodes.append(int(attrs["ref"]))
 		elif name == "member":
+			if self._object == None: return
+
 			member = Member()
 			if attrs.has_key("role"): member.role = attrs["role"]
 			if attrs["type"] == "way":
@@ -97,17 +117,23 @@ class FancyCounter(handler.ContentHandler):
 			print "Unknown element:", name
 
 	def endElement(self, name):
-		# Store in Berkeley DB
-		if name == "node":
-			node_id = node_handler.createNode(self._object)
-			raw_spatial_data.write("%f:%f:%d\n"%(self._object.lat, self._object.lon, node_id))
-		elif name == "way":
-			way_handler.createWay(self._object)
-		elif name == "relation":
-			relation_handler.createRelation(self._object)
-
 		if self._elems % 20000 == 0:
 			print "Processed %d elements" % self._elems
+
+		# Store in Berkeley DB
+		if name == "node":
+			if self._object == None: return
+
+			if node_partitioner.from_node(self._object)[0] == THIS_SERVER:
+				node_id = node_handler.createNode(self._object)
+				raw_spatial_data.write("%f:%f:%d\n"%(self._object.lat, self._object.lon, node_id))
+		elif name == "way":
+			if self._object == None: return
+			way_handler.createWay(self._object)
+		elif name == "relation":
+			if self._object == None: return
+			relation_handler.createRelation(self._object)
+
 
 
 	def endDocument(self):
