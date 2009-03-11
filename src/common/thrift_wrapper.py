4e06fbed190b17eb6ce2f4c6ee5d5411eebb624a
jakin <jason.kivlighn@gmail.com>
2009-03-11 13:03:00 -0700

    Use TBinaryProtocolAccelerated for serializing/deserializing thrift objects.

--- src/common/thrift_wrapper.py
+++ src/common/thrift_wrapper.py
@@ -1,18 +1,17 @@
+# -*- coding: utf-8 -*-
 from cStringIO import StringIO
 
 from thrift.protocol import TBinaryProtocol
 from thrift.transport import TTransport
 
+protocol_factory = TBinaryProtocol.TBinaryProtocolAcceleratedFactory()
+
 def to_string(thrift_obj):
-	buf = StringIO()
-	transport = TTransport.TFileObjectTransport(buf)
-	protocol = TBinaryProtocol.TBinaryProtocol(transport)
-	thrift_obj.write(protocol)
-	return buf.getvalue()
+	trans = TTransport.TMemoryBuffer()
+	prot = protocol_factory.getProtocol(trans)
+	thrift_obj.write(prot)
+	return trans.getvalue()
 
 def from_string(thrift_obj, data):
-	buf = StringIO(data)
-	transport = TTransport.TFileObjectTransport(buf)
-	protocol = TBinaryProtocol.TBinaryProtocol(transport)
-	thrift_obj.read(protocol)
-
+	prot = protocol_factory.getProtocol(TTransport.TMemoryBuffer(data))
+	thrift_obj.read(prot)
